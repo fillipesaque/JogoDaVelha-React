6a399779c7364f086ba9a5de6142f5171582857b
Fillipe Saque <fillipesaque@gmail.com>
2024-10-09 20:32:03 -0300

    JogoDaVelha

---
+++ JogoDaVelha/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Board from './components/board/Board'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='App'>
+        <h1>
+          Jogo da Velha
+        </h1>
+          <Board/>
+      </div>
+    </>
+  )
+}
+
+export default App
